5b657c5dd14696f51bf1efa8ab496d9e733b7175
shvetsovdm <sorrows@mail.ru>
2017-02-18 23:49:50 +1000

    Configure for proper static settings for heroku

--- django_example/settings.py
+++ django_example/settings.py
@@ -108,11 +108,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 X_FRAME_OPTIONS = 'DENY'
