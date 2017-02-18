2e7b63257640aa6b9afb4b9043bf666344a2268b
shvetsovdm <sorrows@mail.ru>
2017-02-18 23:39:19 +1000

    Fix tuple type with STATICFILES_DIRS in settings

--- django_example/settings.py
+++ django_example/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '../contactform/static'))
+STATICFILES_DIRS = ((os.path.join(BASE_DIR, '../contactform/static')),)
 
 X_FRAME_OPTIONS = 'DENY'
 CSRF_COOKIE_HTTPONLY = True
